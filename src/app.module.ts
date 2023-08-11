a856b2faea12e20efcd7190902d13385f6139062
Pedro Ismael Jara <pedro.jara@naranjax.com>
2023-08-11 11:27:12 -0300

    feat: Mongo and Postgre models / Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '@app/auth';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
